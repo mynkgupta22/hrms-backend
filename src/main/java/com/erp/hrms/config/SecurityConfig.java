b80511b306d766507e90c8ee1fa20f62cb0b1738
mynkgupta22 <mayankmk22@gmail.com>
2025-04-27 00:46:38 +0530

    chnagesa added

--- src/main/java/com/erp/hrms/config/SecurityConfig.java
+++ src/main/java/com/erp/hrms/config/SecurityConfig.java
@@ -7,52 +7,44 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserDetailsServiceImpl userDetailsService;
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
     private final String[] WHITE_LIST_URLS = {
-            "/register-company", "/authenticate", "/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**"
+            "/register-company","/authenticate","/swagger-ui/**","/v3/api-docs/**","/api-docs/**"
     };
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-
-        // Explicitly set the userDetailsService and passwordEncoder for authenticationManager
-        authenticationManagerBuilder.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder);
-
-        return authenticationManagerBuilder.build();
+    AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder).and().build();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers(WHITE_LIST_URLS).permitAll()
-                                .requestMatchers(HttpMethod.POST, "/add-users").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+                .cors().disable()
+                .csrf().disable()
+                .authorizeHttpRequests(e -> {
+                    e.requestMatchers(WHITE_LIST_URLS).permitAll()
+                            .requestMatchers(HttpMethod.POST,"/add-users").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .sessionManagement(e->e.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
